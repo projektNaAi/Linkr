9ddcf20a04eb346bbae89e8ae49c1dfe74a57dd7
id1001 <ij44335@zut.edu.pl>
2022-12-12 23:44:45 +0100

    Wygenerowanie encji User na nowo, wykorzystujac make:user
    W celu zapewnienia zgodnosci z komponentem Security.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,23 +6,22 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $nazwa = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $haslo = null;
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $username = null;
 
     #[ORM\Column]
-    private ?bool $czy_jest_adminem = null;
+    private array $roles = [];
 
     #[ORM\OneToMany(mappedBy: 'user_id', targetEntity: Link::class)]
     private Collection $links;
@@ -32,43 +31,69 @@ class User
         $this->links = new ArrayCollection();
     }
 
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getNazwa(): ?string
+    public function getUsername(): ?string
     {
-        return $this->nazwa;
+        return $this->username;
     }
 
-    public function setNazwa(string $nazwa): self
+    public function setUsername(string $username): self
     {
-        $this->nazwa = $nazwa;
+        $this->username = $username;
 
         return $this;
     }
 
-    public function getHaslo(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->haslo;
+        return (string) $this->username;
     }
 
-    public function setHaslo(string $haslo): self
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        $this->haslo = $haslo;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
 
-    public function isCzyJestAdminem(): ?bool
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
-        return $this->czy_jest_adminem;
+        return $this->password;
     }
 
-    public function setCzyJestAdminem(bool $czy_jest_adminem): self
+    public function setPassword(string $password): self
     {
-        $this->czy_jest_adminem = $czy_jest_adminem;
+        $this->password = $password;
 
         return $this;
     }
@@ -101,5 +126,14 @@ class User
         }
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
