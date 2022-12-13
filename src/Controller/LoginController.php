69d73983622f5da89c3ff05fae11b15fe8289c2d
id1001 <ij44335@zut.edu.pl>
2022-12-13 10:23:14 +0100

    Logowanie i rejestracja WIP 1

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser())
+            return new Response('<b>Jestes juz zalogowany</b>');
+
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
