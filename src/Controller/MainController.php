c5086ee8f422555f6a3a06d6b318a365b91482cd
id1001 <ij44335@zut.edu.pl>
2022-12-20 11:26:06 +0100

    Przekierowanie

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,11 +3,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\LinkRepository;
 
 class MainController extends AbstractController
 {
     public function index(): Response
     {
         return $this->render('base.html.twig');
+    }
+
+    public function show(string $shortcut, LinkRepository $linkRepository): Response
+    {
+        if (empty($shortcut))
+            return $this->redirectToRoute('main');
+
+        $link = $linkRepository->findOneBy(['shortLink' => $shortcut]);
+        if (!$link)
+            return $this->redirectToRoute('main');
+
+        return $this->redirect($link->getLongLink());
     }
 }
