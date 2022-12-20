71edfa6e0542d843e42f7ba62a58ce738d6dcb06
rm46627 <rm46627@zut.edu.pl>
2022-12-20 10:54:04 +0100

    usuwanie konta

--- src/Controller/ManageAccountController.php
+++ src/Controller/ManageAccountController.php
@@ -6,15 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use App\Form\ChangeLoginFormType;
 use App\Form\ChangePasswordFormType;
+use App\Form\RemoveAccountFormType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
 
 class ManageAccountController extends AbstractController {
     // public function index(): Response {
     //     return $this->render('manageAccount.html.twig');
     // }
 
-    public function changeLoginPassword(Request $request,UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function changeLoginPassword(Request $request,UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SessionInterface $session): Response
     {
         $user = $this->getUser();
         
@@ -23,6 +26,9 @@ class ManageAccountController extends AbstractController {
 
         $passwordForm = $this->createForm(ChangePasswordFormType::class, $user);
         $passwordForm->handleRequest($request);
+
+        $removeForm = $this->createForm(RemoveAccountFormType::class, $user);
+        $removeForm->handleRequest($request);
 
         if ($loginForm->isSubmitted() && $loginForm->isValid()) {
             $user->setUsername($loginForm->get('username')->getData());
@@ -47,9 +53,20 @@ class ManageAccountController extends AbstractController {
             return $this->redirectToRoute('main');
         }
 
+        if ($removeForm->isSubmitted() && $removeForm->isValid()) {
+            
+            $entityManager->remove($user);
+            $entityManager->flush();
+            
+            $request->getSession()->invalidate();
+            $this->container->get('security.token_storage')->setToken(null);
+            return $this->redirectToRoute('app_logout');
+        }
+
         return $this->render('manageAccount.html.twig', [
             'changeLoginForm' => $loginForm->createView(),
             'changePasswordForm' => $passwordForm->createView(),
+            'removeAccountForm' => $removeForm->createView(),
         ]);
     }
 }
